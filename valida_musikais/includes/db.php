c5aae95e2e087cc83809cab60a4c5f2cbcea5739
johntheo <john@MacBook-Pro-de-John.local>
2015-03-12 00:33:10 -0300

    Alterações na base

--- valida_musikais/includes/db.php
+++ valida_musikais/includes/db.php
@@ -1,7 +1,7 @@
 <?php 
-$DB_HOST = '127.0.0.1';
-$DB_USER = 'root';
-$DB_PASS = 'admin';
-$DB_NAME = 'angularcode_task';
+$DB_HOST = 'musikais.com';
+$DB_USER = 'johnt376_mus';
+$DB_PASS = 'pass123';
+$DB_NAME = 'johnt376_musikais';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
